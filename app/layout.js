f3daa4aed0ec782ae3df594d81beb830f7cd492f
mikash91 <mihailo_jesic@yahoo.com>
2023-05-31 15:23:38 +0200

    nav

--- app/layout.js
+++ app/layout.js
@@ -13,47 +13,56 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }) {
+  const navList = [
+    [`Glavna Strana`, `/`],
+    [`O Nama`, `/o_nama`],
+    [`Usluge`, `/usluge`],
+    [`Projekti`, `/Projekti`],
+    [`Kontakt`, `/Kontakt`],
+  ];
+
+  function generateList(list) {
+    let output = [];
+
+    list.forEach((el) => {
+      output.push(
+        <li>
+          <Link href={`${el[1]}`} className={layout.navLink}>
+            {el[0]}
+          </Link>
+        </li>
+      );
+    });
+
+    return output;
+  }
+
   return (
     <html lang="en">
       <body className={inter.className}>
-        <nav>
+        <nav className={layout.navBar}>
           <div className={layout.logoContainer}>
             <Image src={logo} alt="Struktor Logo" sizes="25vw" />
           </div>
-          <div>
-            <ul>
-              <li>
-                <Link href={`#`} className={layout.navLink}>
-                  glavna strana
-                </Link>
-              </li>{" "}
-              <li>
-                <Link href={`#`} className={layout.navLink}>
-                  o nama
-                </Link>
-              </li>{" "}
-              <li>
-                <Link href={`#`} className={layout.navLink}>
-                  usluge
-                </Link>
-              </li>{" "}
-              <li>
-                <Link href={`#`} className={layout.navLink}>
-                  projekti
-                </Link>
-              </li>{" "}
-              <li>
-                <Link href={`#`} className={layout.navLink}>
-                  kontakt
-                </Link>
-              </li>
-            </ul>
+          <div className={layout.navLinkContainer}>
+            <ul className={layout.navList}>{generateList(navList)}</ul>
           </div>
         </nav>
         {children}
-        <nav>
-          <h1>Goodbye Site</h1>
-        </nav>
+        <div className={layout.contact}>
+          <ul className={layout.contactList}>
+            <li>Telefon 1: xxx/xxxx-xxx</li>
+            <li>Telefon 2: xxx/xxxx-xxx</li>
+          </ul>{" "}
+          <ul className={layout.contactList}>
+            <li>Adressa 1: Ulica XYZ</li>
+            <li>Adressa 2: Ulica XYZ</li>
+          </ul>{" "}
+          <ul className={layout.contactList}>
+            <li>Site 1: example.com</li>
+            <li>Site 2: example.com</li>
+          </ul>
+        </div>
       </body>
     </html>
   );
