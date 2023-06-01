4837bd8403ffe73f075bc5ed92facaedf98a15cd
mikash91 <mihailo_jesic@yahoo.com>
2023-06-01 16:05:21 +0200

    routes

--- app/layout.js
+++ app/layout.js
@@ -2,7 +2,7 @@ import "./globals.css";
 import Image from "next/image";
 import Link from "next/link";
 import layout from "./layout.module.css";
-import logo from "../public/Struktor Jesic.png";
+import logo from "../public/Struktor Logo.png";
 import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -17,8 +17,8 @@ export default function RootLayout({ children }) {
     [`Glavna Strana`, `/`],
     [`O Nama`, `/o_nama`],
     [`Usluge`, `/usluge`],
-    [`Projekti`, `/Projekti`],
-    [`Kontakt`, `/Kontakt`],
+    [`Projekti`, `/projekti`],
+    [`Kontakt`, `/kontakt`],
   ];
 
   function generateList(list) {
@@ -42,7 +42,17 @@ export default function RootLayout({ children }) {
       <body className={inter.className}>
         <nav className={layout.navBar}>
           <div className={layout.logoContainer}>
-            <Image src={logo} alt="Struktor Logo" sizes="25vw" />
+            <Image
+              src={logo}
+              className={layout.logo}
+              alt="Struktor Logo"
+              sizes="25vw"
+            />
+            <span className={layout.struktor}>
+              STRUKTOR JEŠIĆ
+              <br />
+              D.O.O.
+            </span>
           </div>
           <div className={layout.navLinkContainer}>
             <ul className={layout.navList}>{generateList(navList)}</ul>
--- app/usluge/page.js
+++ app/usluge/page.js
@@ -4,7 +4,7 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <main className={styles.main}>
-      <h1>Glavna Strana</h1>
+      <h1>Usluge</h1>
     </main>
   );
 }
