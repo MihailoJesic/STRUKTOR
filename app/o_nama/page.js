ecf94757c02e489506ff5e3cc2303a73db289129
mikash91 <mihailo_jesic@yahoo.com>
2023-06-20 16:00:24 +0200

    about

--- app/o_nama/page.js
+++ app/o_nama/page.js
@@ -5,39 +5,54 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <h1 className="title">O nama</h1>
-      <div>
-        <div>
-          <div>
+      <div className={styles.container}>
+        <div className={styles.textContainer}>
+          <p className={styles.text}>
             Kompanija &rdquo;STRUKTOR JEŠIĆ&rdquo; D.O.O. tri decenije uspešno
             posluje u oblasti građevinske infrastrukture. Od 1993. godine u
             kontinuitetu radi se na razvoju delatnosti, izradi kompleksnih
             projekata, stambenih i industrijskih celina.{" "}
-          </div>
-          <div>
+          </p>
+          <p className={styles.text}>
             Primarna delatnost firme obuhvata kvalitetno i odgovorno izvođenje
             građevinskih radova uz upotrebu atestiranih sirovina i uz nadzor
             stručnog kadra.
-          </div>
-          <div>
+          </p>
+          <p className={styles.text}>
             Na osnovu zahteva tržišta, u saradnji sa laboratorijom za
             ispitivanje betona u opseg svog poslovanja uvrstili smo proizvodnju
             sveže betonske mešavine, sa transportom i isporukom na teritoriji
             Zlatiborskog okruga.
-          </div>
-          <div>
+          </p>
+          <p className={styles.text}>
             Stručni kadar čine građevinski inženjeri i kvalifikovani građevinski
             radnici koji organizovani u timove sprovode najkompleksnije projekte
             od zemljanih do završnih radova.
-          </div>
-          <div>
+          </p>
+          <p className={styles.text}>
             Kao odgovorni izvođači radova, svoje projekte u celini sprovodimo
             prema važećim odredbama Zakona o planiranju i izgradnji, uz izradu
             neophodne prateće dokumentacije.
-          </div>
-          <div>
+          </p>
+          <p className={styles.text}>
             Naš cilj je da svojim klijentima realizujemo zahteve na dobar i
             pouzdan način uz garanciju kvaliteta izvedenih radova.
-          </div>
+          </p>
+        </div>
+        <div className={styles.imageContainer}>
+          <a
+            href="/o nama/o-nama-1.jpg"
+            target="_blank"
+            className={styles.imageLink}
+          >
+            <Image
+              src="/o nama/o-nama-1.jpg"
+              className={styles.image}
+              alt="Struktor Offices"
+              sizes="100%"
+              fill={true}
+            />{" "}
+          </a>
         </div>
       </div>
     </main>
--- app/projekti/page.js
+++ app/projekti/page.js
@@ -6,83 +6,83 @@ export default function Home() {
   const galleryData = [
     {
       image: "/projekti/images (1).jpg",
-      altText: "Betonska baza",
+      altText: "Struktor Projekat",
     },
     {
       image: "/projekti/images (2).jpg",
-      altText: "Betonska baza",
+      altText: "Struktor Projekat",
     },
     {
       image: "/projekti/images (3).jpg",
-      altText: "Betonska baza",
+      altText: "Struktor Projekat",
     },
     {
       image: "/projekti/images (4).jpg",
-      altText: "Betonska baza",
+      altText: "Struktor Projekat",
     },
     {
       image: "/projekti/images (5).jpg",
-      altText: "Betonska baza",
+      altText: "Struktor Projekat",
     },
     {
       image: "/projekti/images (6).jpg",
-      altText: "Betonska baza",
+      altText: "Struktor Projekat",
     },
     {
       image: "/projekti/images (7).jpg",
-      altText: "Betonska baza",
+      altText: "Struktor Projekat",
     },
     {
       image: "/projekti/images (8).jpg",
-      altText: "Betonska baza",
+      altText: "Struktor Projekat",
     },
     {
       image: "/projekti/images (9).jpg",
-      altText: "Betonska baza",
+      altText: "Struktor Projekat",
     },
     {
       image: "/projekti/images (10).jpg",
-      altText: "Betonska baza",
+      altText: "Struktor Projekat",
     },
     {
       image: "/projekti/images (11).jpg",
-      altText: "Betonska baza",
+      altText: "Struktor Projekat",
     },
     {
       image: "/projekti/images (12).jpg",
-      altText: "Betonska baza",
+      altText: "Struktor Projekat",
     },
     {
       image: "/projekti/images (13).jpg",
-      altText: "Betonska baza",
+      altText: "Struktor Projekat",
     },
     {
       image: "/projekti/images (14).jpg",
-      altText: "Betonska baza",
+      altText: "Struktor Projekat",
     },
     {
       image: "/projekti/images (15).jpg",
-      altText: "Betonska baza",
+      altText: "Struktor Projekat",
     },
     {
       image: "/projekti/images (16).jpg",
-      altText: "Betonska baza",
+      altText: "Struktor Projekat",
     },
     {
       image: "/projekti/images (17).jpg",
-      altText: "Betonska baza",
+      altText: "Struktor Projekat",
     },
     {
       image: "/projekti/images (18).jpg",
-      altText: "Betonska baza",
+      altText: "Struktor Projekat",
     },
     {
       image: "/projekti/images (19).jpg",
-      altText: "Betonska baza",
+      altText: "Struktor Projekat",
     },
     {
       image: "/projekti/images (20).jpg",
-      altText: "Betonska baza",
+      altText: "Struktor Projekat",
     },
   ];
 
